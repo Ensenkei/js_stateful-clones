17a7ae8a94df5ed6e19a3d74b6f35165782732b1
Oleksandr Polishchuk <crazy8diamond@gmail.com>
2025-04-02 12:29:15 +0300

    Addition to Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -21,6 +21,8 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         clear();
         break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     stateClones.push({ ...stateCopy });
